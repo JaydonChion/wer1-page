206c6315d9e6d1ed5f92b461b3b780614444f92c
jaydonchion <chionjetherng@gmail.com>
2023-01-14 21:27:36 +0800

    ADD: Whatsapp

---
+++ js/whatsapp.js
@@ -0,0 +1,10 @@
+$(function () {
+$('#whatsappDiv').floatingWhatsApp({
+    phone: '6565144693',
+    popupMessage: 'Hello, how can we help you?',
+    message: "Hi Wer1, I would like to know more about the services",
+    showPopup: true,
+    showOnIE: false,
+    headerTitle: 'Chat with us',
+});
+});
