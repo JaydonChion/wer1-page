c674ce7c7dd6b9b6024df2a74751969857885532
Jet Jet Boi <34904336+JaydonChion@users.noreply.github.com>
2023-02-13 14:25:40 +0800

    Update promocode.js

--- js/promocode.js
+++ js/promocode.js
@@ -1,7 +1,7 @@
 $(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const param = urlParams.get('promo');
-  if(param.length > 0){x
+  if(param.length > 0){
        $("#promocodeDiv").show();
        $("input#promocode").val(param);
        $("input#promocode").attr('readonly', true);
