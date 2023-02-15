5ae3c3049e159097efdb828df426bf5dc5c5a926
jaydonchion <chionjetherng@gmail.com>
2023-02-15 21:50:47 +0800

    ADD: promo t&c

--- js/promocode.js
+++ js/promocode.js
@@ -3,6 +3,7 @@ $(function () {
   const param = urlParams.get('promo');
   if(param !== null && param.length > 0){
        $("#promoApplied").show();
+       $("#promotandc").show();
        $("input#promocode").val(param);
        $("input#promocode").attr('readonly', true);
   }
