60ed3b83031fb0b494aeef53d10165eba0934c18
jaydonchion <chionjetherng@gmail.com>
2023-02-15 22:16:16 +0800

    UPD: formatting of t and c

--- js/promocode.js
+++ js/promocode.js
@@ -2,6 +2,7 @@ $(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const param = urlParams.get('promo');
   if(param !== null && param.length > 0){
+       $("#promoApplied").text("Promo code <".concat(param ,"> is applied successfully"))
        $("#promoApplied").show();
        $("#promotandc").show();
        $("input#promocode").val(param);
